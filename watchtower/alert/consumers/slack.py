f4fd313641c85ef1373460c0c155929cc5bbd7da
Alistair King <alistair@caida.org>
2019-10-17 11:43:53 -0700

    WIP on slack plugin

--- watchtower/alert/consumers/slack.py
+++ watchtower/alert/consumers/slack.py
@@ -1,33 +1,112 @@
 import logging
+import slack
 
 from . import AbstractConsumer
 
 
+MSG_JSON = """{
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*{name}*"
+			},
+            "accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Show in Dashboard",
+					"emoji": true
+				},
+				"url": "https://ioda.caida.org/ioda/dashboard#view=inspect&entity={meta_type}/{meta_code}&lastView=overview"
+			}
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": "*Type:* {position}"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "*{meta_type}*: {meta_code}"
+				}
+			]
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"fields": [
+				{
+					"type": "mrkdwn",
+					"text": "*Current Value:* {actual}"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "*Predicted Value:* {predicted}"
+				},
+				{
+					"type": "mrkdwn",
+					"text": "*Relative Drop:* {pct_drop}%"
+				}
+			]
+		},
+		{
+			"type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": "{alert_time}"
+				}
+			]
+		}
+	]
+}
+"""
+
+
 class SlackConsumer(AbstractConsumer):
 
-    loggers = {
-        'normal': logging.info,
-        'warning': logging.warn,
-        'critical': logging.error,
+    defaults = {
+        'api_token': None,
+        'channel': None
     }
 
-    def handle_alert(self, alert):
-        log_str = "ALERT: %s %s %d (%s)" %\
-                  (alert.level.upper(), alert.name, alert.time,
-                   alert.expression)
-        self.loggers[alert.level](log_str)
+    def __init__(self, config):
+        super(SlackConsumer, self).__init__(self.defaults)
+        if config:
+            self.config.update(config)
+        self.channel = None
+        self.client = None
 
-        for v in alert.violations:
-            log_str = "VIOLATION: %s Time: %d %s Value: %s History Value: %s" %\
-                      (alert.level.upper(), alert.time, v.expression, v.value,
-                       v.history_value)
-            self.loggers[alert.level](log_str)
+    def start(self):
+        self.channel = self.config['channel']
+        self.client = slack.WebClient(token=self.config['api_token'])
+
+    def handle_alert(self, alert):
+        alert.annotate_violations()
+        for viol in alert.violations:
+            rel_drop = None
+            if viol.history_value is not None and viol.value is not None:
+                rel_drop = (viol.history_value - viol.value) / viol.history_value * 100
+            msg_str = MSG_JSON.format(
+                name=alert.name,
+                meta_type=viol.meta['meta_type'] if 'meta_type' in viol.meta else "",
+                meta_code=viol.meta['meta_code'] if 'meta_code' in viol.meta else "",
+                position="Back to Normal" if alert.level == 'normal' else "Outage Alert",
+                actual=viol.value,
+                predicted=viol.history_value,
+                pct_drop=rel_drop,
+                alert_time=viol.time
+            )
+            logging.info(msg_str)
 
     def handle_error(self, error):
-        log_str = "ERROR: %s %s %d %s %s" % (error.type, error.name,
-                                             error.time, error.expression,
-                                             error.message)
-        logging.error(log_str)
+        pass
 
     def handle_timer(self, now):
-        logging.info("TIMER: periodic timer fired at %d" % now)
+        pass
