179830a06a51b452c64b9120f00fe2329fd0aa88
Alistair King <alistair@caida.org>
2019-10-29 11:00:40 -0700

    Add text to slack message

--- watchtower/alert/consumers/slack.py
+++ watchtower/alert/consumers/slack.py
@@ -23,24 +23,6 @@ class SlackConsumer(AbstractConsumer):
         self.channel = self.config['channel']
         self.client = slack.WebClient(token=self.config['api_token'])
 
-    def _post(self, msg_blocks):
-        retries = 5
-        while retries > 0:
-            try:
-                self.client.chat_postMessage(
-                    channel=self.channel,
-                    blocks=msg_blocks)
-            except SlackApiError as e:
-                if e.response['error'] == 'ratelimited':
-                    retries -= 1
-                    if retries == 0:
-                        raise e
-                    logging.warning("Hit slack rate limit. Waiting 30s.")
-                    time.sleep(30)
-                    continue
-                raise e
-            return
-
     @staticmethod
     def _build_dashboard_url(meta_type, meta_code, from_time, until_time):
         return "https://ioda.caida.org/ioda/dashboard#view=inspect" \
@@ -50,8 +32,6 @@ class SlackConsumer(AbstractConsumer):
     def _build_msg_blocks(self, name, meta_type, meta_code,
                           from_time, until_time, position,
                           actual, predicted, pct_drop, alert_time):
-        predicted_str = "%d" % predicted if predicted is not None else "Unknown"
-        pct_drop_str = "%.2f%%" % pct_drop if pct_drop is not None else "Unknown"
         return [
             {
                 "type": "section",
@@ -91,11 +71,11 @@ class SlackConsumer(AbstractConsumer):
                     },
                     {
                         "type": "mrkdwn",
-                        "text": "*Predicted Value:* %s" % predicted_str
+                        "text": "*Predicted Value:* %s" % predicted
                     },
                     {
                         "type": "mrkdwn",
-                        "text": "*Relative Drop:* %s" % pct_drop_str
+                        "text": "*Relative Drop:* %s" % pct_drop
                     }
                 ]
             },
@@ -113,26 +93,64 @@ class SlackConsumer(AbstractConsumer):
             },  # divider
         ]
 
+    def _build_msg_text(self, name, meta_type, meta_code,
+                        from_time, until_time, position,
+                        actual, predicted, pct_drop, alert_time):
+        # "Outage Start: Telescope source IPs (UCSD-NT)\nAsn: 13317\n
+        return "%s: %s\n" \
+               "%s: %s\n" \
+               "Current Value: %d, Predicted Value: %s, Relative Drop: %s" \
+               "%s" % (position, name,
+                       meta_type.title(), meta_code,
+                       actual, predicted, pct_drop,
+                       alert_time)
+
+    def _post(self, msg_blocks, msg_text):
+        retries = 5
+        while retries > 0:
+            try:
+                self.client.chat_postMessage(
+                    channel=self.channel,
+                    blocks=msg_blocks,
+                    text=msg_text,
+                )
+            except SlackApiError as e:
+                if e.response['error'] == 'ratelimited':
+                    retries -= 1
+                    if retries == 0:
+                        raise e
+                    logging.warning("Hit slack rate limit. Waiting 30s.")
+                    time.sleep(30)
+                    continue
+                raise e
+            return
+
+    def _send_msg(self, msg_details):
+        msg_blocks = self._build_msg_blocks(**msg_details)
+        msg_text = self._build_msg_text(**msg_details)
+        self._post(msg_blocks, msg_text)
+
     def handle_alert(self, alert):
         alert.annotate_violations()
         for viol in alert.violations:
-            # TODO: move this logic inside build_msg method
             rel_drop = None
             if viol.history_value is not None and viol.value is not None:
                 rel_drop = (viol.history_value - viol.value) / viol.history_value * 100
-            msg_blocks = self._build_msg_blocks(
-                name=alert.name,
-                meta_type=viol.meta['meta_type'] if 'meta_type' in viol.meta else "",
-                meta_code=viol.meta['meta_code'] if 'meta_code' in viol.meta else "",
-                from_time=viol.time - 8*3600,
-                until_time=viol.time + 8*3600,
-                position="Outage End" if alert.level == 'normal' else "Outage Start",
-                actual=viol.value,
-                predicted=viol.history_value,
-                pct_drop=rel_drop,
-                alert_time=time.strftime('%m/%d/%Y %H:%M:%S UTC',  time.gmtime(viol.time))
-            )
-            self._post(msg_blocks)
+            predicted_str = "%d" % viol.history_value if viol.history_value is not None else "Unknown"
+            pct_drop_str = "%.2f%%" % rel_drop if rel_drop is not None else "Unknown"
+            details = {
+                "name": alert.name,
+                "meta_type": viol.meta['meta_type'] if 'meta_type' in viol.meta else "",
+                "meta_code": viol.meta['meta_code'] if 'meta_code' in viol.meta else "",
+                "from_time": viol.time - 8 * 3600,
+                "until_time": viol.time + 8 * 3600,
+                "position": "Outage End" if alert.level == 'normal' else "Outage Start",
+                "actual": viol.value,
+                "predicted": predicted_str,
+                "pct_drop": pct_drop_str,
+                "alert_time": time.strftime('%m/%d/%Y %H:%M:%S UTC', time.gmtime(viol.time)),
+            }
+            self._send_msg(details)
 
     def handle_error(self, error):
         pass
